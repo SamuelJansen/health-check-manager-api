ef63c1f70a5176ac17501fa465667feab1b74b24
samuel.jansen <samuel.jansenn@gmail.com>
2021-11-07 04:46:30 -0300

    feature: preparing for api-key

--- api/src/HealthCheckManagerApi.py
+++ api/src/HealthCheckManagerApi.py
@@ -1,4 +1,4 @@
 from python_framework import ResourceManager
 import ModelAssociation
 
-api, app, jwt = ResourceManager.initialize(__name__, ModelAssociation.MODEL)
+app = ResourceManager.initialize(__name__, ModelAssociation.MODEL)
--- app.py
+++ app.py
@@ -1,28 +1,4 @@
-import globals
-globalsInstance = globals.newGlobalsInstance(__file__
-    , settingStatus = True
-    , successStatus = True
-    , errorStatus = True
-    , failureStatus = True
-    , infoStatus = True
-    , statusStatus = True
-    # , debugStatus = True
-    #
-    # , warningStatus = True
-    # , wrapperStatus = True
-    # , logStatus = True
-    # , testStatus = True
-)
-
-from python_framework import initialize, runApi
-import HealthCheckManagerApi
-app = HealthCheckManagerApi.app
-api = HealthCheckManagerApi.api
-jwt = HealthCheckManagerApi.jwt
-
-@initialize(api, defaultUrl = '/swagger', openInBrowser=False)
-def runFlaskApplication(app):
-    runApi(debug=False, use_reloader=False)
-
-if __name__ == '__main__' :
-    runFlaskApplication(app)
+from python_framework import newApp, runApi
+app = newApp(__file__)
+if __name__ == '__main__':
+    runApi()
